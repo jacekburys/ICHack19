09ebd24b26fbb56be503d2ae569eadb6b76c6086
Richard Rother <sorex@dyn904-28.eduroam.ic.ac.uk>
2019-01-27 02:35:35 +0000

    added backend

---
+++ backend/root/be/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
+from django.views import View
+from PIL import Image
+
+class BEView(View):
+    def get(self, request):
+        return HttpResponse("hello world")
+
+    def post(self, request):
+        print(request.FILES)
+        if 'myImage' in request.FILES:
+            print("it's here yo")
+            print(request.FILES['myImage'])
+            im = Image.open(request.FILES['myImage'])
+            width, height = im.size
+            # DO PROCESSING
+            dimRes = "Width: " + str(width) + ", Height: " + str(height) + "\n"
+
+        return HttpResponse(dimRes)
